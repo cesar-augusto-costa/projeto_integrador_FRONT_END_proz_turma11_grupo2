7782d2f8bc661acd8ea15d8575eee7c5c4e7f4a7
Eliane <elimadasilva@yahoo.com.br>
2023-09-22 18:12:37 -0300

    aterações conteudo e formatação pag adoção

--- js/adocao.js
+++ js/adocao.js
@@ -1,52 +1,58 @@
-/*criação de variáveis*/
-const slide = document.querySelectorAll(".slide");
-const botaoVoltar = document.getElementById("botao-voltar");
-const botaoAvancar = document.getElementById("botao-avancar");
-/*constante para o slide que está ativo*/
-let correnteSlide = 0;
+/*codigo quiz*/
+let skippular = document.getElementById('skip_pular');
+let scorepontuacao = document.getElementById('score_pontuacao');
+let totalScore = document.getElementById('totalScore');
+let countdowncontagem = document.getElementById('countdown_contagem');
+let count = 0;
+let scoreCount = 0;
+let duration = 0;
+let qasettitulo = document.querySelectorAll('.qa_set_titulo');
+let qaAnsRow = document.querySelectorAll('.qa_set_titulo .qa_ans_row_linha input');
+skippular.addEventListener('click',function(){
+    step();
+    duration =10
+})
 
-/*criação de funções*/
-/*função para esconder o slider*/
+qaAnsRow.forEach( function(qaAnsRowSingle){
+    qaAnsRowSingle.addEventListener('click', function(){
+        setTimeout(function(){
+            step();
+            duration =10
+        },500)
+        var valid = this.getAttribute("valid");
+        if(valid == "valid"){
+            scoreCount = scoreCount + 20;
+            scorepontuacao.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        }else{
+            scoreCount = scoreCount - 20;
+            scorepontuacao.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
 
-function esconderSlider(){
-    slide.forEach(item => item.classList.remove("on"));
-}
+        }
+    })
+})
 
-/*função mostrar o slider*/
-
-function mostrarSlider(){
-    slide[correnteSlide].classList.add('on');
-
-}
-
-function avancarSlide(){
-    esconderSlider();
-    if(correnteSlide == slide.length -1){
-        correnteSlide == 0;
-    }else{
-        correnteSlide++;
+function step(){
+    count += 1;
+    for(let i = 0; i < qasettitulo.length; i++){
+        qasettitulo[i].className ='qa_set_titulo';
     }
+    qasettitulo[count].className = 'qa_set_titulo active';
+    if(count == 6){
+        skippular.style.display = 'none';
+        clearInterval(durationTime);
+        countdowncontagem.innerHTML = 0;
 
-    mostrarSlider();
+    }
 }
 
-function voltarSlide(){
-    esconderSlider();
-    if(correnteSlide == 0){
-        correnteSlide == slide.length -1;
-    }else{
-        correnteSlide--;
+let durationTime = setInterval(function(){
+    if(duration == 10){
+        duration = 0;
     }
+   
+},1000);
 
-    mostrarSlider();
-}
 
-//evento nos botões
-botaoAvancar.addEventListener('click', avancarSlide);
-botaoVoltar.addEventListener('click', voltarSlide);
 
-//Esse código javascript procura todos os links e adiciona o parâmetro target="_blank". Ele faz com que o link seja aberto em outra aba.
-var links = document.getElementsByClassName("ancora")
-    for (var i=0, len=links.length; i < len; i ++) {
-        links[i].target = '_blank';
-    }
