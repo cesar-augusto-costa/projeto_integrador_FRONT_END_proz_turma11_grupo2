ef3cf41aed65ec9256a2a96a717130f58a7105af
Ozorio Travassos <skiperd2015@hotmail.com>
2023-10-16 19:37:28 -0300

    mudanca concluido

--- js/assinatura.js
+++ js/assinatura.js
@@ -14,12 +14,12 @@ class ValidarFormulario {
     if (camposvalidos) {
       window.alert('Cadastro efetuado');
       this.formulario.submit();
-      window.location.replace('http://127.0.0.1:5500/pages/concluido.html');
+      window.location.href = 'concluido.html';
     }
     console.log('Formulário não enviado');
   }
   camposValidos() {
-    let valid = true;
+    let valido = true;
     for (let errorMsg of this.formulario.querySelectorAll('.mensagem-erro')) {
       errorMsg.remove();
     }
@@ -27,30 +27,30 @@ class ValidarFormulario {
       if (!campo.value) {
         const label = campo.previousElementSibling.innerText;
         this.criaErro(campo, `${label} não pode está vazio`);
-        valid = false;
+        valido = false;
       }
       if (campo.classList.contains('cpf')) {
-        if (!this.validaCpf(campo)) valid = false;
+        if (!this.validaCpf(campo)) valido = false;
       }
       if (campo.classList.contains('cartao')) {
         if (!this.validaNumero(campo)) return false;
       }
     }
-    return valid;
+    return valido;
   }
 
   validaNumero(campo) {
     const cartao = campo.value;
-    let valid = true;
+    let valido = true;
     if (cartao.length < 13 || cartao.length > 16) {
       this.criaErro(campo, 'Informe os digitos do cartão');
-      valid = false;
+      valido = false;
     }
     if (!cartao.match(/^[0-9]+/g)) {
       this.criaErro(campo, 'Número precisa conter apenas digitos');
-      valid = false;
+      valido = false;
     }
-    return valid;
+    return valido;
   }
 
   validaCpf(campo) {
